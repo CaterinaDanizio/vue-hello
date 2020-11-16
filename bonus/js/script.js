13b8362a2d09c8f39dd84b4ddaaba9897407ed80
Caterina <danizio@outlook.it>
2020-11-16 22:57:23 +0100

    Style and fine tuning

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -3,28 +3,23 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Abiti da sera in promozione',
-    caption: 'Abito prom in raso in maglia con paillettes',
+    message: ' Abiti da sera in promozione ',
+    caption: ' Abito prom in raso in maglia con paillettes ',
     img: "img/abito1.jpg"
   },
 
   // Altro possibile Bonus: gestire una funzione con un evento nel modo Vue.
   methods: {
   forward: function () {
-      if(this.caption === "Abito prom in raso in maglia con paillettes")   {
-        this.caption = "Abito prom a fessura con spalle scoperte con volant";
+      if(this.caption === " Abito prom in raso in maglia con paillettes ")   {
+        this.caption = " Abito prom a fessura con spalle scoperte con volant ";
         this.img = "img/abito2.jpg";
           }
 
-    else if (this.caption === "Abito prom a fessura con spalle scoperte con volant") {
-      this.caption = "Abito arricciato con fasce in vita con nodo ampio";
-      this.img = "img/abito3.jpg";
+    else if (this.caption === " Abito prom a fessura con spalle scoperte con volant ") {
+      this.caption = " Abito arricciato con fasce in vita con nodo ampio ";
+      this.img = "img/abito3.jpg ";
         }
-    else {
-      this.caption = "Abito prom in raso in maglia con paillettes";
-      this.img = "img/abito1.jpg";
-         }
-
     }
   }
 });
