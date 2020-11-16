bdf1eced15741ab64058a70ce4452934bbef0373
Caterina <danizio@outlook.it>
2020-11-16 15:56:51 +0100

    Stampa titolo con Vue

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// CONSEGNA
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Ciao a tutti da Vue!'
+  }
+});
